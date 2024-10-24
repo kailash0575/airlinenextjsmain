8afe0a5fa43a8a0dbb3e45534f218c5c4ce13c8b
Kailash Tanwar <kailash.tanwar@acedigitalsolution.com>
2024-10-24 13:44:17 +0530

    commit

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -1,7 +1,6 @@
 export const metadata = {
   title: "Dashboard",
-  description:
-    "Read the disclaimer of AirportAirlinesTerminal, and understand that we are a third-party travel website, providing details on airport terminals. ",
+ 
 };
 
 export default function AdminLayout({ children }) {

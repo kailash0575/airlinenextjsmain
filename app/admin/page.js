c3d7a43425c75a39dcbefd1c4514de83f7acfb51
Kailash Tanwar <kailash.tanwar@acedigitalsolution.com>
2024-12-05 12:48:57 +0530

    change height of  input_comtainer

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,5 +1,11 @@
 import AdminSideBar from "@/components/AdminSideBar";
-import Admin from '../../components/Admin'
+// import Admin from '../../components/Admin'
+import dynamic from "next/dynamic";
+
+const Admin = dynamic(() => import("@/components/Admin"), {
+  ssr: false,
+});
+
 const page = () => {
   console.log("admin");
   return (
